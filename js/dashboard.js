239d3dae0cf4c9b4c6817d8b0df327bde9f12f45
simon <simon.dal-ponte8@etu.univ-lorraine.fr>
2020-02-10 13:04:37 +0100

    ajout requirejs

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,16 +9,16 @@ shim: {
 'core': ['bootstrap', 'jquery'],
 },
 paths: {
-'core': 'assets/js/core',
-'jquery': 'assets/js/vendors/jquery-3.2.1.min',
-'bootstrap': 'assets/js/vendors/bootstrap.bundle.min',
-'sparkline': 'assets/js/vendors/jquery.sparkline.min',
-'selectize': 'assets/js/vendors/selectize.min',
-'tablesorter': 'assets/js/vendors/jquery.tablesorter.min',
-'vector-map': 'assets/js/vendors/jquery-jvectormap-2.0.3.min',
-'vector-map-de': 'assets/js/vendors/jquery-jvectormap-de-merc',
-'vector-map-world': 'assets/js/vendors/jquery-jvectormap-world-mill',
-'circle-progress': 'assets/js/vendors/circle-progress.min',
+'core': 'core',
+'jquery': 'vendors/jquery-3.2.1.min',
+'bootstrap': 'vendors/bootstrap.bundle.min',
+'sparkline': 'vendors/jquery.sparkline.min',
+'selectize': 'vendors/selectize.min',
+'tablesorter': 'vendors/jquery.tablesorter.min',
+'vector-map': 'vendors/jquery-jvectormap-2.0.3.min',
+'vector-map-de': 'vendors/jquery-jvectormap-de-merc',
+'vector-map-world': 'vendors/jquery-jvectormap-world-mill',
+'circle-progress': 'vendors/circle-progress.min',
 }
 });
 window.tabler = {
